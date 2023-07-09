1ec16b82b4d9255a879cdb028cfd11f903129304
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-09 13:12:19 +0300

    4th ex solution

--- ex4.py
+++ ex4.py
@@ -5,6 +5,9 @@
 # ✔ Решение в одну строку.
 
 
-int_iterator = [item for item in range(0, 100) if ((item % 10) + (item - (item % 10)) / 10) != 8]
+int_iterator = (item for item in range(0, 100) if (((item % 10) + (item - (item % 10)) / 10) != 8) and (item % 2) == 0)
 
 print(int_iterator)
+for i in int_iterator:
+    print(i)
+
